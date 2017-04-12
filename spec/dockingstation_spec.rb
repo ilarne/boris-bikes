5b375ba680e456bffcb6b5fd962f0aa339657294
isabellarner <larner.isabel@gmail.com>
2017-04-12 13:42:28 +0100

    Step 10

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -4,3 +4,7 @@ describe DockingStation do
   it { is_expected.to be_a DockingStation}
   it { is_expected.to respond_to(:release_bike)}
 end
+
+describe DockingStation.new.release_bike do
+  it { is_expected.to be_a Bike}
+end
