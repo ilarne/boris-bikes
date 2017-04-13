5d1bb311af86601021ea368f1c550635ba83095b
isabellarner <larner.isabel@gmail.com>
2017-04-13 07:51:05 +0100

    Step 11+

--- lib/boris_bike.rb
+++ lib/boris_bike.rb
@@ -1,15 +1,14 @@
 class DockingStation
-  attr_reader :bike_count
-  def initialize
-    @bike_count = 10
-  end
+    attr_reader :bike
 
   def release_bike
     Bike.new
   end
 
-  def dock
+  def dock(bike)
+    @bike = bike
   end
+
 end
 
 class Bike
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -2,14 +2,18 @@ require 'boris_bike'
 
 describe DockingStation do
   it { is_expected.to be_a DockingStation}
-  it { is_expected.to respond_to(:release_bike)}
-  it { is_expected.to respond_to(:dock)}
+  it { is_expected.to respond_to(:release_bike) }
+  it { is_expected.to respond_to(:dock) }
+  it { is_expected.to respond_to(:dock).with(1).argument }
+  it {is_expected.to respond_to(:bike) }
+
+it 'expects bike' do
+bike = Bike.new
+  expect(subject.dock(bike)).to eq bike
 end
 
 describe DockingStation.new.release_bike do
   it { is_expected.to be_a Bike}
 
-  it 'releasing bike raises an error' do
-    expect {(:release_bike)}.to raise_error
   end
 end
